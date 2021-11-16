c2d56a19cede5c3b1b23f8b1ecae2685c4e065ec
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2021-11-16 15:08:06 -0800

    Fixes #20

--- CharacterSelection/game.hpp
+++ CharacterSelection/game.hpp
@@ -1,22 +1,42 @@
 #ifndef GAME_HPP
 #define GAME_HPP
+#include "CharacterSelection.hpp"
 
 #include <iostream>
 
 class Game
 {
     private:
-
-
-
-
-
+        int gameState;
+        Character* user;
     
     public:
+        void screenUpdate();
+        void newGame();
+        
 
 
 
 };
+
+void Game::screenUpdate()
+{
+    
+}
+
+
+void Game::newGame()
+{
+    char userInput;
+    user = new Character();
+    std::cout << "Welcome Prompt Message! It's time to create your character!" << '\n' << "[Press any Key To Continue...]";
+
+
+
+
+
+
+}
 
 
 #endif GAME_HPP
