05208ec88f6f77a16f0bbbb28705ed8b2fcf2088
faduckie <faduckabdullah@gmail.com>
2021-11-15 23:25:36 -0800

    added gtest and cmake file

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include "longSword.hpp"
 #include "staff.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
     Weapons weapon("Sword");
     weapon.setBehavior(new LongSword);
 
     for(int i = 0; i < 5; i++)
         weapon.attack(1);
+
+    return 0;
     
 }
