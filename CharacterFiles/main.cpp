22780f9d07e54ee47219225db2d5b62c78fa427f
Victor <87928341+VMSandoval25@users.noreply.github.com>
2021-11-17 03:37:56 -0800

    updating character files

---
+++ CharacterFiles/main.cpp
@@ -0,0 +1,15 @@
+#include "attackCharacter.hpp"
+#include <iostream>
+
+int main()
+{
+    Attribute wizardStats(10,8,12,10);
+    AttackChacracter wizard(wizardStats);
+    wizard.setName ("Wizard");
+
+    std::cout<<"You have chosen the "<<wizard.getName ()<<std::endl;
+    wizard.printVitals();
+    printf("----------------------------\n");
+    wizard.increaseEXP(12);
+    //wizard.printVitals();
+}
