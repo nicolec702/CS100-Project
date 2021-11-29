a382e5a5560a6f84749f30135ef245f9f68c4b40
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2021-11-29 03:16:20 -0800

    INT_MAX pt.3

--- helperFunction.cpp
+++ helperFunction.cpp
@@ -1,8 +1,6 @@
 #include "helperFunction.hpp"
-#ifdef __unix__
-#define int INT_MAX = 2147483647
-#endif
 
+const int INT_MAX = 2147483647
 
 int verifyIntInput(std::string input) // returns -1 if inproper input
 {
