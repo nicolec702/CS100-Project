c134f38cec0c9a43fcbd66643516ba72fa1f2e1b
Victor <87928341+VMSandoval25@users.noreply.github.com>
2021-11-30 04:22:44 -0800

    Adding test for character classes

---
+++ Unit Tests/characterTest.cpp
@@ -0,0 +1,88 @@
+
+#include "../BaseGameFiles/helperFunction.cpp"
+#include "../WeaponsFiles/weapons.cpp"
+#include "../WeaponsFiles/weaponBehavior.hpp"
+#include "../WeaponsFiles/longSword.hpp"
+#include "../WeaponsFiles/dagger.hpp"
+#include "../WeaponsFiles/staff.hpp"
+#include "../WeaponsFiles/broom.hpp"
+#include "../CharacterFiles/attribute.cpp"
+#include "../CharacterFiles/attributeBehavior.hpp"
+#include "../CharacterFiles/wizard.hpp"
+#include "../CharacterFiles/rogue.hpp"
+#include "../CharacterFiles/bob.hpp"
+#include "../CharacterFiles/knight.hpp"
+#include "../CharacterFiles/attackCharacter.hpp"
+#include "../CharacterFiles/character.hpp"
+#include "../CharacterFiles/playableCharacter.hpp"
+#include "../CharacterFiles/npAttackCharacter.hpp"
+#include "gtest/gtest.h"
+
+TEST(PlayableCharacter, verifyName){
+    
+    PlayableCharacter *player = new PlayableCharacter();
+    player->setPlayerName("Player 1")
+    EXPECT_EQ(player->getPlayerName(), "Player 1");
+    delete player;
+}
+TEST(PlayableCharacter, verifyAttackKnight){
+    
+    PlayableCharacter *player = new PlayableCharacter();
+    player->setPlayerName("Knight")
+    player->setCharacterType (2);
+    EXPECT_EQ(player->getAttack(), 15);
+    delete player;
+}
+
+TEST(PlayableCharacter, verifyNameChange){
+    
+    PlayableCharacter *player = new PlayableCharacter();
+    player->setPlayerName("Player 1")
+    player->setPlayerName ("Hello");
+    EXPECT_EQ(player->getPlayerName(), "Hello");
+    delete player;
+}
+
+TEST(Character, verifyName){
+    Character *character = new Character();
+    character->setName("Narrator");
+    EXPECT_EQ(character->getName(), "Narrator");
+    delete character;
+}
+
+TEST(Character, verifyNameChange){
+    Character *character = new Character();
+    character->setName("Narrator");
+    EXPECT_EQ(character->getName(), "Narrator");
+    delete character;
+}
+
+TEST(NPAttackCharacter, verifyAttackWizard)
+{
+    NPAttackCharacter *wizard = new NPAttackCharacter(1,"Wizard",1,1);
+    EXPECT_EQ(wizard->getAttack(), 15);
+    delete wizard;
+}
+
+TEST(NPAttackCharacter, verifyDefkWizard)
+{
+    NPAttackCharacter *wizard = new NPAttackCharacter(1,"Wizard",1,1);
+    EXPECT_EQ(wizard->getDefense(), 11);
+    delete wizard;
+}
+
+TEST(NPAttackCharacter, verifyHPkWizard)
+{
+    NPAttackCharacter *wizard = new NPAttackCharacter(1,"Wizard",1,1);
+    EXPECT_EQ(wizard->getHp(), 15);
+    delete wizard;
+}
+
+
+
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
